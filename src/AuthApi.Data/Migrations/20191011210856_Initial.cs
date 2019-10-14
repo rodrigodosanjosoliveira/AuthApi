644ff0a559eba4d28db8baee5faedc8285c166ee
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2019-10-14 01:56:46 -0300

    EF Core migrations

---
+++ src/AuthApi.Data/Migrations/20191011210856_Initial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthApi.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: true),
+                    Nome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true),
+                    Token = table.Column<string>(nullable: true),
+                    UltimoLogin = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Telefone",
+                columns: table => new
+                {
+                    Ddd = table.Column<string>(nullable: false),
+                    Numero = table.Column<string>(nullable: false),
+                    UsuarioId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telefone", x => new { x.UsuarioId, x.Ddd, x.Numero });
+                    table.ForeignKey(
+                        name: "FK_Usuarios",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Telefone");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
