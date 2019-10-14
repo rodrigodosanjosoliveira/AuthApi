4c1dce14633e673f948d47f592e6d1f37b267ce1
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2019-10-14 02:00:20 -0300

    Application services

---
+++ src/AuthApi.Application/Services/UsuarioService.cs
@@ -0,0 +1,67 @@
+﻿using AuthApi.Domain.Contracts.Repositories;
+using AuthApi.Domain.Contracts.Services;
+using AuthApi.Domain.Dto;
+using AuthApi.Domain.Entities;
+using AuthApi.Domain.ValueTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthApi.Application.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public async Task<Usuario> Create(UsuarioInputDto usuarioInput)
+        {
+            var telefones = usuarioInput.Telefones.Select(t => new Telefone(t.Ddd, t.Numero,t.UsuarioId));
+            var usuario = new Usuario(usuarioInput.Nome, usuarioInput.Email, usuarioInput.Senha, telefones.ToList())
+            {
+                Token = usuarioInput.Token
+            };
+            Usuario novoUsuario = await _usuarioRepository.Create(usuario);
+            return novoUsuario;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            await _usuarioRepository.Delete(id);
+        }
+
+        public Erro EmailJaExiste(UsuarioInputDto usuario)
+        {
+            Erro erro = null;
+
+            if (GetAll().Any(p => p.Email == usuario.Email))
+                erro = new Erro { StatusCode = "400", Mensagem = "Parâmetros inválidos." };
+
+            return erro;
+        }
+
+        public IEnumerable<Usuario> GetAll()
+        {
+            var usuarios = _usuarioRepository.GetAll();
+
+            return usuarios;
+        }
+
+        public async Task<Usuario> GetById(Guid id)
+        {
+            return await _usuarioRepository.GetById(id);
+        }
+
+        public async Task<Usuario> Update(Guid id, UsuarioDto usuario)
+        {
+            var usuarioDb = await GetById(id);
+            var usuarioAtualizado = new Usuario(usuario.Nome, usuario.Email, usuario.Senha, usuario.Telefones.Select(t => new Telefone(t.Ddd, t.Numero, usuario.Id)).ToList());
+            return await _usuarioRepository.Update(id, usuarioAtualizado);
+        }
+    }
+}
